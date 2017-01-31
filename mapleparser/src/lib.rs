50511411b655a3869d465ec79b44064d72338af7
S.Percentage <Syn.Tri.Naga@gmail.com>
2017-01-31 11:31:17 +0900

    added TokenRef

--- mapleparser/src/lib.rs
+++ mapleparser/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate rustc_serialize;
 
 mod lexer;
+mod parser;
 
 pub use lexer::*;
+pub use parser::*;
---
+++ mapleparser/src/parser.rs
@@ -0,0 +1,14 @@
+// Parser
+
+use {Token, TokenSubtype};
+
+pub struct TokenRef<'s>
+{
+	container: &'s [Token], ptr: usize
+}
+impl<'s> TokenRef<'s>
+{
+	pub fn new(c: &'s [Token]) -> Self { TokenRef { container: c, ptr: 0 } }
+	pub fn current(&self) -> &'s Token { self.peek(0) }
+	pub fn peek(&self, offset: usize) -> &'s Token { if self.ptr + offset >= self.container.len() { self.container.last().unwrap() } else { &self.container[self.ptr + offset] } }
+}
